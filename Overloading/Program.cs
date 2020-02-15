de8f376197212f8e34c18427b0648d9c5f6b2923
Andrei Turcanu <Andrei Turcanu@DESKTOP-B0LQIU9>
2020-02-15 17:13:25 +0200

    changed one comment

--- Overloading/Program.cs
+++ Overloading/Program.cs
@@ -8,27 +8,27 @@ namespace Overloading
         static void Main(string[] args)
         {
             Angle a1 = new Angle(12, 25, 30);
-            //Angle a2 = new Angle(0, 600, 7200);
-            //Angle a3 = new Angle(0, 600, 7217);
+            Angle a2 = new Angle(10, 10, 15);
+            Angle a3 = new Angle(15, 27, 17);
 
             Angle a7 = new Angle(15, 27, 36);
 
-            Angle minus = a1 - a7;
-            Console.WriteLine(a1);
-            Console.WriteLine(a7);
-            Console.WriteLine(minus);
+            //Angle minus = a1 - a7;
+            //Console.WriteLine(a2);
+            //Console.WriteLine(a7);
+            //Console.WriteLine(minus);
 
             Angle[] angles = new Angle[3];
             angles[0] = a1;
-            //angles[1] = a2;
-            //angles[2] = a3;
+            angles[1] = a2;
+            angles[2] = a3;
 
-            //Angle.SortAngles(angles);
-           
-            //foreach(var i in angles)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            Angle.SortAngles(angles);
+
+            foreach (var i in angles)
+            {
+                Console.WriteLine(i);
+            }
 
             //Angle a5 = a1 + a3;
 
@@ -40,8 +40,9 @@ namespace Overloading
             //Console.WriteLine(a1 == a2);
 
 
-            Angle a9 = a1 * 3;
+            //Angle a9 = a1 * 3;
 
+            //Console.WriteLine(a9);
         }
 
     }
