91859711e43194c8e9191acdbdcae21b68a265c4
Andrei Turcanu <Andrei Turcanu@DESKTOP-B0LQIU9>
2020-02-18 17:45:19 +0200

    some changes

--- Overloading/Program.cs
+++ Overloading/Program.cs
@@ -14,31 +14,28 @@ namespace Overloading
 
             Angle a7 = new Angle(15, 27, 36);
 
-            a3.Seconds = 120;
 
-            Console.WriteLine(a3);
+            Console.WriteLine(a3*=4);
 
 
-            Angle minus = a1 + a7;
-            //Console.WriteLine(a2);
-            //Console.WriteLine(a7);
-            Console.WriteLine(minus);
+            //Console.WriteLine(a1 + a7);
 
-            Console.WriteLine(a1-a3);
-            Angle[] angles = new Angle[5];
+
+            //Console.WriteLine(a1-a3);
+            Angle[] angles = new Angle[4];
             angles[0] = a1;
             angles[1] = a2;
             angles[2] = a3;
             angles[3] = a7;
-            angles[4] = minus;
 
 
-            //Angle.SortAngles(angles);
 
-            //foreach (var i in a3)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            Angle.SortAngles(angles);
+
+            foreach (var i in angles)
+            {
+                Console.WriteLine(i);
+            }
 
             //Angle a5 = a1 + a3;
 
